4c2009c04f93eb7c9e4409063f0fbf1db5d56eb6
miknivin <116654570+miknivin@users.noreply.github.com>
2024-03-18 19:31:26 +0530

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,23 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDatabase = ()=>{
-    let DB_URI = ""
+export const connectDatabase = () => {
+  let DB_URI = "";
 
-    if (process.env.NODE_ENV==='DEVELOPMENT') {
-        DB_URI = process.env.DB_LOCAL_URI
-        //console.log(DB_URI);
-    }
+  if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
+  if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
 
-    if (process.env.NODE_ENV==='PRODUCTION') {
-        DB_URI= process.env.DB_URI
-        //console.log(DB_URI);
-    }
-
-    mongoose.connect(DB_URI).then((c)=>{
-        console.log(`Mongo db connected with HOST: ${c?.connection?.host}`);
-    })
-    .catch((err)=>{
-        console.log('Mongodb connection error'+err);
-    })
-}
-
+  mongoose.connect(DB_URI).then((con) => {
+    console.log(
+      `MongoDB Database connected with HOST: ${con?.connection?.host}`
+    );
+  });
+};
